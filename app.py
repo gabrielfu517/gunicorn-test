0b593a41ecfbfab0e87bb4d44dc0ad117aca7e89
gabriel-fu <gabriel.fu@barkmall.com>
2022-11-17 12:04:04 +0800

    test

--- app.py
+++ app.py
@@ -6,56 +6,71 @@ import shopify
 from datetime import date
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
+from flask_apscheduler import APScheduler
 from helpers import *
 
 # Configure application
 app = Flask(__name__)
 
-# Get customer list
-shop_url = "https://e9d82dadea8353e21e5788f4e3cfde61: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@barkmall.myshopify.com/admin"
-shopify.ShopifyResource.set_site(shop_url)
-shop = shopify.Shop.current
+# Flask hook for cron job
+scheduler = APScheduler()
 
-customers = get_all_resources(shopify.Customer)
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
 
-# Push to dotdigital
-today = str(date.today())
+@scheduler.task('interval', id='sync_wishlist_cron', seconds=86400)
+def sync_cron():
 
-payload = []
-index = 0
+    # Get customer list
+    shop_url = "https://e9d82dadea8353e21e5788f4e3cfde61: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@barkmall.myshopify.com/admin"
+    shopify.ShopifyResource.set_site(shop_url)
+    shop = shopify.Shop.current
 
-for customer in customers:
+    customers = get_all_resources(shopify.Customer)
 
-    index += 1
+    # Push to dotdigital
+    today = str(date.today())
 
-    if customer.email != None:
+    payload = []
+    index = 0
 
-        # Get wishlist
-        url = 'https://www.barkmall.com/apps/wishlist'
+    for customer in customers:
 
-        params = dict(
-            type='api',
-            customerid=customer.id,
-            version='1'
-        )
+        index += 1
 
-        resp = requests.get(url=url, params=params)
-        data = resp.json()
+        if customer.email != None:
 
-        new_data = {
-            "key": "wishlist" + '-' + today + get_random_string(10),
-            "contactIdentifier": customer.email,
-            "json": data
-        }
-        payload.append(new_data)
-        print(str(index) + '/' + str(len(customers)) + ' email - ' +customer.email)
+            # Get wishlist
+            url = 'https://www.barkmall.com/apps/wishlist'
 
-url = "https://r3-api.dotdigital.com/v2/contacts/transactional-data/import/shopify_wishlist"
-headers = {
-    "accept": "application/json",
-    "content-type": "application/json",
-    "authorization": "Basic YXBpdXNlci03YzYzN2ZlZDBkMTFAYXBpY29ubmVjdG9yLmNvbToxR29AMTA3QENleE4="
-}
+            params = dict(
+                type='api',
+                customerid=customer.id,
+                version='1'
+            )
 
-response = requests.post(url, json=payload, headers=headers)
-print(response.text)
+            resp = requests.get(url=url, params=params)
+            data = resp.json()
+
+            new_data = {
+                "key": "wishlist" + '-' + today + get_random_string(10),
+                "contactIdentifier": customer.email,
+                "json": data
+            }
+            payload.append(new_data)
+            print(str(index) + '/' + str(len(customers)) + ' email - ' +customer.email)
+
+    url = "https://r3-api.dotdigital.com/v2/contacts/transactional-data/import/shopify_wishlist"
+    headers = {
+        "accept": "application/json",
+        "content-type": "application/json",
+        "authorization": "Basic YXBpdXNlci03YzYzN2ZlZDBkMTFAYXBpY29ubmVjdG9yLmNvbToxR29AMTA3QENleE4="
+    }
+
+    print(payload)
+    response = requests.post(url, json=payload, headers=headers)
+    print(response.text)
+
+scheduler.init_app(app)
+scheduler.start()
+app.run()
